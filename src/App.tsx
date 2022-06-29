00a3cf5180647be538ece8545f85e6d2394fa795
yty0643 <80657819+yty0643@users.noreply.github.com>
2022-06-29 21:17:00 +0900

    create flow chart item(box) drag and drop logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import styles from './App.module.css';
+import Box from './components/box/box';
 
 function App() {
   return (
-    <div className="App">
-      d
+    <div className={styles.app}>
+      <Box />
     </div>
   );
 }
 
 export default App;
-// (amplify-cjbZ3)
-// https://us-east-1.console.aws.amazon.com/iam/home?region=us-east-1#/users$new?step=final&accessKey&userNames=amplify-cjbZ3&permissionType=policies&policies=arn:aws:iam::aws:policy%2FAdministratorAccess-Amplify
